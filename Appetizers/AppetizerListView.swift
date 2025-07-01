32941c32e6e91792ca1e15ac2cde7fd9e317b39f
Armanul Momin <armanul.momin@gmail.com>
2025-07-01 22:58:44 +1000

    Connecting to UI

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    @State var appetizers: [Appetizer] = []
     var body: some View {
         NavigationView{
-            List(MockData.appetizers)
+            List(appetizers)
             {appetizer in
                 
                 AppetizerListCell(appetizer: appetizer)
                 
             }.navigationTitle("Appetizers")
         }
+        .onAppear{
+            getAppetizers()
+        }
+        
         
     }
+    func getAppetizers(){
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async{
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            
+        }
+    }
+    
 }
 
 #Preview {
--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -8,8 +8,8 @@ import Foundation
 final class NetworkManager{
     static let shared = NetworkManager()
     
-    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals"
-    private let apperizerURL = baseURL + "/appetizers"
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    private let apperizerURL = baseURL + "appetizers"
     private init(){}
     
     func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void)
@@ -19,7 +19,7 @@ final class NetworkManager{
             return 
         }
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-            guard let _ = error else{
+            if let _ = error {
                 completed(.failure(.unableToComplete))
                 return
             }
